a7ea3f814610a4c64abbc927d3d3b22561be4084
luciobo <luciobo@icloud.com>
2023-02-16 17:19:40 +0100

    mile 4 raggiunta

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <script>
-	export default {
+import axios from "axios";
 
-	}
+export default {
+  data() {
+    return {
+      mioUrl: "http://localhost:8000",
+      project: [],
+    };
+  },
+  methods: {
+    fetchProject() {
+      axios.get(this.mioUrl + "/api/projects").then((resp) => {
+        this.project = resp.data;
+      });
+    },
+  },
+  beforeMount() {},
+  mounted() {
+    this.fetchProject();
+  },
+}
 </script>
 
 <template>
   <div>
     <h1>funziona?</h1>
-  </div>
+</div>
 </template>
 
-<style lang="scss">
-	@use "styles/main";
-</style>
+<style lang="scss">@use "styles/main";</style>
